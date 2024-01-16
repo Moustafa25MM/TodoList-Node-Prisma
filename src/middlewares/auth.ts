d26f7a725dc330ec6dc816652188fa9ba7ab8433
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-01-16 23:36:04 +0200

    isAuth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+import { models } from '../models';
 
 dotenv.config();
 const JWTSecret = process.env.JWT_SECRET;
@@ -11,6 +13,34 @@ type TokenPayload = {
 const generateJWT = (payload: TokenPayload): String =>
   jwt.sign(payload, JWTSecret as string, { expiresIn: '7d' });
 
+const isAuthenicated = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const token = request.headers.authorization;
+
+  if (!token) {
+    return response
+      .status(401)
+      .json({ error: 'Unauthorized: Token not provided' });
+  }
+
+  try {
+    const payload: { id: string } = jwt.verify(token, JWTSecret as string) as {
+      id: string;
+    };
+    const existingUser = await models.User.findById(payload.id);
+    if (!existingUser) {
+      return response.status(400);
+    }
+    next();
+  } catch (error) {
+    return response.status(401).json({ error: 'Unauthorized: Invalid token' });
+  }
+};
+
 export const authMethods = {
   generateJWT,
+  isAuthenicated,
 };
